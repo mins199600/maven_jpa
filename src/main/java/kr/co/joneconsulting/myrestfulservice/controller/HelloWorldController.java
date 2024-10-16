ef4b5e1d03262c197a4611cdfa5a890b5131b3da
sumin <mins19960091@gmail.com>
2024-10-16 17:40:08 +0900

    http get 메소드 구현

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package kr.co.joneconsulting.myrestfulservice.controller;
 
 import kr.co.joneconsulting.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,9 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World!");
+    }
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World %s", name));
     }
 }
