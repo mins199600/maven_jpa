25bfbfd1659f6bd9d44005f032764d7b7c2df5da
sumin <mins19960091@gmail.com>
2024-10-20 19:09:37 +0900

    삭제 기능 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
@@ -44,4 +44,13 @@ public class UserController {
 
          return ResponseEntity.created(location).build();
     }
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity deleteUser(@PathVariable int id){
+        User deletedUser = service.deleteById(id);
+
+        if(deletedUser == null){
+            throw new UserNotFoundException(String.format("id[%s] not found", id));
+        }
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/kr/co/joneconsulting/myrestfulservice/dao/UserDaoService.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/dao/UserDaoService.java
@@ -4,6 +4,7 @@ import kr.co.joneconsulting.myrestfulservice.bean.User;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Component
@@ -37,6 +38,20 @@ public class UserDaoService {
     public User findOne(int id){
         for(User user : users){
             if (user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    //삭제
+    public User deleteById(int id){
+        Iterator<User> iterator = users.iterator();
+        while(iterator.hasNext()){
+            User user = iterator.next();
+
+            if(user.getId() == id){
+                iterator.remove();
                 return user;
             }
         }
