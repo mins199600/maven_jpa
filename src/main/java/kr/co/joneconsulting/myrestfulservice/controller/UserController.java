7108522c1fb0caa35a306ee8d6b57884a0c8757f
sumin <mins19960091@gmail.com>
2024-10-20 17:58:46 +0900

    예외발생

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/UserController.java
@@ -2,17 +2,23 @@ package kr.co.joneconsulting.myrestfulservice.controller;
 
 import kr.co.joneconsulting.myrestfulservice.bean.User;
 import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import kr.co.joneconsulting.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
 public class UserController {
+    private final UserDaoService userDaoService;
     private UserDaoService service;
 
-    public UserController(UserDaoService service) {
+    public UserController(UserDaoService service, UserDaoService userDaoService) {
         this.service = service;
+        this.userDaoService = userDaoService;
     }
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
@@ -20,7 +26,22 @@ public class UserController {
     }
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
-    }
+        User user = service.findOne(id);
 
+        if(user == null){
+            throw new UserNotFoundException(String.format("id[%s] not found", id));
+        }
+        return user;
+    }
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+         User savedUser = service.save(user);
+
+         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                 .path("/{id}")
+                 .buildAndExpand(savedUser.getId())
+                 .toUri();
+
+         return ResponseEntity.created(location).build();
+    }
 }
